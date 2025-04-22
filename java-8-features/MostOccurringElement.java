dfb44b5362d38aa8dd8cf76477250c0ac7d427a7
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2025-04-22 09:01:51 +0530

    added Map related changes for java8

--- java-8-features/MostOccurringElement.java
+++ java-8-features/MostOccurringElement.java
@@ -1,7 +1,18 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MostOccurringElement {
     public static void main(String[] args) {
-       // List
+        List<String> words = Arrays.asList("apple", "orange", "banana", "apple", "orange", "apple");
+
+        Map.Entry<String, Long> mostFrequent =  words.stream()
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting() ))
+                .entrySet().stream().max(Map.Entry.comparingByValue()).orElse(null);
+
+        System.out.println( mostFrequent.getKey());
+
+
     }
 }
